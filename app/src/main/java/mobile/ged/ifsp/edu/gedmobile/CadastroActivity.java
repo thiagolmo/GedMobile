206a6e70ed54273140f820515b324a8e66a1b586
Thiago Luiz <thiagoluiz@outlook.com>
2015-06-07 18:36:41 -0300

    Algumas alterações no layout tela cadastro

--- app/src/main/java/mobile/ged/ifsp/edu/gedmobile/CadastroActivity.java
+++ app/src/main/java/mobile/ged/ifsp/edu/gedmobile/CadastroActivity.java
@@ -1,12 +1,13 @@
 package mobile.ged.ifsp.edu.gedmobile;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by Thiago on 07/06/2015.
  */
-public class CadastroActivity extends ActionBarActivity {
+public class CadastroActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
