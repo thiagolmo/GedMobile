440aab02c0a4dd25242e24fbb41ea193715af8ea
Thiago Luiz <thiagoluiz@outlook.com>
2015-06-07 11:27:48 -0300

    Initial commit

---
+++ app/src/main/java/mobile/ged/ifsp/edu/gedmobile/HeloActivity.java
@@ -0,0 +1,15 @@
+package mobile.ged.ifsp.edu.gedmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Thiago on 30/05/2015.
+ */
+public class HeloActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helo);
+    }
+}
